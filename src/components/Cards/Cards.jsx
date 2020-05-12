5dd640fa67d379f88438ae912e541974cee90fcb
Sweta Shree <sristi016@gmail.com>
2020-05-12 10:56:41 +0530

    Cards UI

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,7 +11,7 @@ return 'Loading...'
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item conponent={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+        <Grid item  xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom> Infected </Typography>
             <Typography variant="h5">
@@ -22,7 +22,7 @@ return 'Loading...'
           </CardContent>
         </Grid>
 
-        <Grid item conponent={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom> Recovered </Typography>
             <Typography variant="h5">
@@ -33,14 +33,14 @@ return 'Loading...'
           </CardContent>
         </Grid>
 
-        <Grid item conponent={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom> Deceased </Typography>
             <Typography variant="h5">
             <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
             </Typography>
             <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-             <Typography variant="body2">Number of deaths by COVID-19 </Typography>
+             <Typography variant="body2">Number of deaths caused by COVID-19 </Typography>
           </CardContent>
         </Grid>
       </Grid>
